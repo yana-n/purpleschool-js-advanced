5b5ccad8f6b62a36e4c5fa412d23d7282563f5ef
yana <lins.yana10@gmail.com>
2024-07-29 12:18:05 +0300

    simplify unique func

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const names = [
 	{ id: 1, name: 'Peter' },
 ]
 
-const getUniqueItems = (arr, key = 'id') => {
-	return [...new Set(arr.map(el => el[key]))]
-			.map(uniqueKey =>
-				arr.find(el => el[key] === uniqueKey)
-			)
-}
+const getUniqueItems = arr => new Set(arr.map((el) => arr.find((item) => el.id === item.id)))
 
 console.log(getUniqueItems(names))
 // output: [ { id: 1, name: 'Peter' }, { id: 2, name: 'John' } ]
